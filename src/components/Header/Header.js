6cf6907807ada70f4a0cba0d58bbe7b545719131
cwright89 <connorwwright1989@gmail.com>
2021-02-18 10:54:16 -0700

    added some basic styling to the Header, added header to app.js and set up links in the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,22 @@
 import { withRouter, Link } from 'react-router-dom'
+import './Header.scss'
 
-const Header = props => {
+const Header = (props) => {
 
     return(
-
+        <header className='header-container'>
+            <h1>Hungree</h1>
+            {props.location.pathname !== '/dash'
+            ? (
+                <nav>
+                <Link to='/dash' className='nav-links'>Dashboard</Link>
+                </nav>
+            ):  
+                <nav>
+                    <Link to='/profile' className='nav-links'>Profile</Link>
+                </nav>
+            }
+        </header>
     )
 }
 
