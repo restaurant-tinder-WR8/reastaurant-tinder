7b43de7fa898bc942a80d2cd9532aa0b749d1fa1
stuartErnstsen <stuarternstsen@gmail.com>
2021-02-18 20:08:41 -0700

    cleaning up to make new test branch

--- server/controllers/friendController.js
+++ server/controllers/friendController.js
@@ -1,12 +1,12 @@
 module.exports = {
     getFriends: (req, res) => {
-        const {id} = req.params;
+        const { id } = req.params;
         const db = req.app.get('db');
 
         db.friend.get_friends(id)
             .then(friends => {
                 res.status(200).send(friends)
-                console.log(friends)
+                console.log('MJW', friends)
             })
             .catch(err => res.status(500).send(err));
     },
