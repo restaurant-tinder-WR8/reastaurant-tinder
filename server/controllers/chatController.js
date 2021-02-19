00d0a1908866cbe63d1d5c5e1723b20b0224b16a
stuartErnstsen <stuarternstsen@gmail.com>
2021-02-19 11:13:20 -0700

    got chat with sockets on dash functioning may need to relocate it later

---
+++ server/controllers/chatController.js
@@ -0,0 +1,19 @@
+module.exports = {
+    getLobbyChat: async (req, res) => {
+        const { lobbyId } = req.params
+        const db = req.app.get('db')
+
+        const chatArr = await db.chat.get_lobby_chat({ lobbyId })
+        // console.log('SDE: chatCtrl:8', chatArr)
+        res.status(200).send(chatArr)
+    },
+    addMessageToLobby: async (req, res) => {
+        const { lobbyId, message } = req.body
+        const { decidee_id } = req.session.user
+        const db = req.app.get('db')
+        console.log('hit')
+        await db.chat.add_message_to_lobby({ lobbyId, message, decidee_id })
+        const newChatArr = await db.chat.get_lobby_chat({ lobbyId })
+        res.status(201).send(newChatArr)
+    }
+}
