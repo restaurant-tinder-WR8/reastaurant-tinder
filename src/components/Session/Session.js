5854cb93e94a67a0e686527d971ac47190fbb5ad
cwright89 <connorwwright1989@gmail.com>
2021-02-19 15:33:57 -0700

    added very basic styling to containers so we can visualize as we build it out

--- src/components/Session/Session.js
+++ src/components/Session/Session.js
@@ -1,15 +1,18 @@
 import axios from "axios";
 import { useState, useEffect, useContext } from "react";
 import AppContext from "../../context/app-context";
+import "./Session.scss"
 
 const Session = () => {
 
     return (
         <div>
             <div className="restaurant-container">
-                <div className="photo-container"/>  
+                <div className="photo-container"/> 
+            <div className="info">   
             <h2>Title of Restaurant</h2>
             <h3>Rating</h3><h3>Cost($$)</h3>
+            </div> 
             </div>
             <button>Stomp</button>
             <button>Chomp</button>
