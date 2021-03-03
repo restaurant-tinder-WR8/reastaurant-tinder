1383ef3eb337eae74a08d002323fc19d50e1b8ef
mattwoodruff808 <mjwoodruff93@gmail.com>
2021-03-03 11:56:04 -0700

    Another fix for hosting

--- server/index.js
+++ server/index.js
@@ -37,7 +37,12 @@ app.use(session({
     resave: true,
     saveUninitialized: false,
     secret: SESSION_SECRET,
-    cookie: { maxAge: 1000 * 60 * 60 * 24 * 365 }
+    cookie: { 
+        maxAge: 1000 * 60 * 60 * 24 * 365,
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none' 
+    }
 }));
 
 massive({
